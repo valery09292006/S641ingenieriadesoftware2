69221c5e3ecb90f38088323e49f060cf0ef2b0cb
valery09292006 <161169652+valery09292006@users.noreply.github.com>
2025-10-03 14:24:35 +0000

    interface

---
+++ PatronBuliderS641Interface/WordConverter.java
@@ -0,0 +1,16 @@
+public class WordConverter implements ConverterI {
+
+    public void makeLine() {
+        System.out.println("Línea con Word");
+
+    }
+
+    public void makeParagraph() {
+        System.out.println("Párrafo con Word");
+    }
+
+    public void makeTable() {
+        System.out.println("Tabla con Word");
+
+    }
+}
